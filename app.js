e7e67de7a7f7495133d5c7e64196b24a3352b775
yooncobra <yooncobra@gmail.com>
2016-09-01 15:37:19 +0900

    initial commit + first node server 노드 첫번째 커밋 내용은 연습으로 작성함

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+//http Status(200): OK, send response "hello world"
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write("hello world \n");
+    response.end();
+}).listen(3030);
+
+console.log("Server is running at localhost:3030");
+
